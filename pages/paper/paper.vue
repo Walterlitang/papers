0a0e6a58f85bff7a4eea0a2fb4ba13bec639d4a8
tangli <tangli@logwirecloud.com>
2025-02-19 14:27:36 +0800

    fix:init project

---
+++ pages/paper/paper.vue
@@ -0,0 +1,26 @@
+<template>
+	<view class="paper_wrap">
+		paper
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style lang="scss">
+.paper_wrap{
+	width: 100vw;
+	height: 100vh;
+	background: pink;
+}
+</style>
